3f940fc633a464a75ee7733ac67e34d101d8f961
Yasser Osama <yosama@youxel.com>
2025-01-14 13:59:54 +0200

    initial commit

---
+++ Sources/MyLibrary/SwiftUIView.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIView.swift
+//  MyLibrary
+//
+//  Created by Yasser Osama on 10/29/24.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    
+    var viewModel: ViewModel
+    
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    public var body: some View {
+        Text(viewModel.title)
+            .padding()
+            .background(viewModel.color)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+@Observable
+public class ViewModel {
+    public var title: String = ""
+    public var color: Color = .blue
+    
+    public init(title: String, color: Color = .blue) {
+        self.title = title
+        self.color = color
+    }
+}
+
+#Preview {
+    SwiftUIView(viewModel: ViewModel(title: "Hello SwiftUI"))
+}
